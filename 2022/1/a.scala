e8d7a077aa54d384dffa95c1f8a63169c996f5dd
eflisback <ebbe.flisback@gmail.com>
2023-09-25 14:24:04 +0200

    Back to .scala files

---
+++ 2022/1/a.scala
@@ -0,0 +1,17 @@
+import scala.collection.mutable.Queue
+
+@main def day1a = {
+  val lines = scala.io.Source.fromFile("2022/1/data.txt").getLines.toArray
+  var sums: Queue[Int] = Queue()
+  var currentSum = 0
+
+  val highestSum = {
+    for (line <- lines)
+      if !line.isEmpty() then currentSum += line.toInt
+      else { sums += currentSum; currentSum = 0 }
+    sums.max
+  }
+
+  println(highestSum)
+
+}
---
+++ 2022/1/b.scala
@@ -0,0 +1,24 @@
+import scala.collection.mutable.Queue
+
+@main def day1b = {
+  // Top n calorie-holders to sum
+  val n = 3
+
+  val lines = scala.io.Source.fromFile("2022/1/data.txt").getLines.toArray
+
+  var sums: Queue[Int] = Queue()
+  var currentSum = 0
+
+  for (line <- lines)
+    if !line.isEmpty() then currentSum += line.toInt
+    else { sums += currentSum; currentSum = 0 }
+
+  sums = sums.sorted(Ordering[Int].reverse)
+  var sum = 0
+
+  for (i <- 0 until n)
+    sum += sums(i)
+
+  println(sum)
+
+}
